5be9a1e48b297eaaf7a06685c89a31cc6e8b5d4a
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-02-21 23:32:34 +0530

    [Kapil Namdeo]Add.UC-2 Hello bridgelabz message in web page

--- src/main/java/com/helloworldspringapp/HelloWorldController.java
+++ src/main/java/com/helloworldspringapp/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.helloworldspringapp;
 
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
+
+    //UC1
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    //UC2
+    @GetMapping("/helloworld")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello from BridgeLabz");
+        return "hello"; // Refers to hello.html in the templates folder
     }
 }
